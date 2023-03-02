21c403dbfb5e896b10ea9a8b028b51355b88337a
dracero <diego.racero@gmail.com>
2023-03-01 21:20:37 -0300

    secoond

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.9643970e.js';
-export const imports = ["_app/immutable/entry/error.svelte.9643970e.js","_app/immutable/chunks/index.f98f3a93.js","_app/immutable/chunks/singletons.cabb284f.js"];
-export const stylesheets = [];
-export const fonts = [];
